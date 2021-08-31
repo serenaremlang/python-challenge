f82567bfbfe9e935293739e11e819f3011332f3c
Serena <serena.rem.lang@gmail.com>
2021-08-30 22:32:28 -0400

    Wrote sudo code for challenge

---
+++ PyBank/main.py
@@ -0,0 +1,29 @@
+#import modules
+import os
+
+#read in datafile
+
+#total number of months in dataset
+
+#net total amount of Profit/Losses over entire period
+
+
+#Change in Profit/Losses over entire period then find average of those changes
+
+
+#greatest increase in profits (date and amount) over the entire period
+
+
+#greatest decrease in profits (date and amount) over entire period
+
+#Export results to text file
+
+
+#Print analysis
+print('Financial Analysis')
+print('-'*30)
+print(f'Total Months: {}')
+print(f'Total: ${}')
+print(f'Average Change: ${}')
+print(f'Greatest Increase in Profits: {} {}')
+print(f'Greatest Decrease in Profits: {} {}')
