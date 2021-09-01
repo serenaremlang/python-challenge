72a27a0e965d18c91f7ab1cbc0d9cf7afa26ad89
Serena <serena.rem.lang@gmail.com>
2021-08-31 20:12:56 -0400

    Read in the file into my script

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,17 @@
 #import modules
 import os
+import csv
 
 #read in datafile
+csvpath = os.path.join('..', 'Resources', 'budger_data.csv')
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
 
 #total number of months in dataset
 
