d1989f445bc88519d0150c717b2b59d23aa14bf4
Serena <serena.rem.lang@gmail.com>
2021-09-03 01:56:29 -0400

    Wrote sudo code for PyPoll challenge

---
+++ PyPoll/main.py
@@ -0,0 +1,21 @@
+# Dependencies
+import os
+import csv
+
+#Define filepath
+
+#Read in the csv
+
+#The total number of votes cast
+
+#A complete list of candidates who received votes
+
+#The percentage of votes each candidate won
+
+#The total number of votes each candidate won
+
+#The winner of the election based on popular vote.
+
+#Write to text file
+
+#Print to Terminal
